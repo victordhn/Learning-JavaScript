1757484a348290495e94ac27590d4507ae3a4d95
victordhn <33136394+victordhn@users.noreply.github.com>
2024-07-14 01:57:05 -0300

    updated

--- pig_game/script.js
+++ pig_game/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//  COMMENT: THIS CODE IS WAY MESSIER THAN IT SHOULD!
+
 const accumulatedScore0 = function (score) {
   document.getElementById('score--0').textContent = score;
 };
@@ -21,7 +23,12 @@ const reset = function () {
   accumulatedScore1(0);
   currentScore0(0);
   currentScore1(0);
-  let [current0, current1, score0, score1, player0, player1] = [0,0,0,0,1,0];
+  gameState.current0= 0;
+  gameState.current1= 0;
+  gameState.score0= 0;
+  gameState.score1= 0;
+  gameState.player0= 1;
+  gameState.player1= 0;
   document.querySelector(`.player--0`).classList.add("player--active");
   document.querySelector(`.player--0`).classList.remove("player--winner");  
   document.querySelector(`.player--1`).classList.remove("player--active","player--winner");
@@ -45,42 +52,49 @@ document.querySelector('.btn--new').addEventListener('click', function () {
   reset();
 });
 
-reset();
+let gameState = {
+  current0: 0,
+  current1: 0,
+  score0: 0,
+  score1: 0,
+  player0: 1,
+  player1: 0,
+}
 
-let [current0, current1, score0, score1, player0, player1] = [0,0,0,0,1,0];
+
+reset();
 
 
 document.querySelector('.btn--roll').addEventListener('click', function () {
   let randomInt = Math.floor(Math.random() * 6 + 1);
   document.querySelector('.dice').src = `dice-${randomInt}.png`;
   document.querySelector('.dice').style.display = '';
-  if (player0 === 1) {
+  if (gameState.player0 === 1) {
     randomInt === 1
-      ? ((current0 = 0), (player0 = 0), (player1 = 1), remove(0), add(1))
-      : (current0 += randomInt);
-    currentScore0(current0);
-  } else if (player1 === 1) {
+      ? ((gameState.current0 = 0), (gameState.player0 = 0), (gameState.player1 = 1), remove(0), add(1))
+      : (gameState.current0 += randomInt);
+    currentScore0(gameState.current0);
+  } else if (gameState.player1 === 1) {
     randomInt === 1
-      ? ((current1 = 0), (player1 = 0), (player0 = 1), remove(1), add(0))
-      : (current1 += randomInt);
-    currentScore1(current1);
+      ? ((gameState.current1 = 0), (gameState.player1 = 0), (gameState.player0 = 1), remove(1), add(0))
+      : (gameState.current1 += randomInt);
+    currentScore1(gameState.current1);
   }
 });
 
 document.querySelector('.btn--hold').addEventListener('click', function () {
   document.querySelector('.dice').style.display = 'none';
-  if (player0 === 1) {
-    score0 += current0;
-    accumulatedScore0(score0);
+  if (gameState.player0 === 1) {
+    gameState.score0 += gameState.current0;
+    accumulatedScore0(gameState.score0);
     currentScore0(0);
-    checkWinner(score0,0)
-    current0 = 0, player0 = 0, player1=1, remove(0), add(1);
-  } else if (player1 === 1) {
-    score1 += current1;
-    accumulatedScore1(score1);
+    checkWinner(gameState.score0,0)
+    gameState.current0 = 0, gameState.player0 = 0, gameState.player1=1, remove(0), add(1);
+  } else if (gameState.player1 === 1) {
+    gameState.score1 += gameState.current1;
+    accumulatedScore1(gameState.score1);
     currentScore1(0);
-    checkWinner(score1,1)
-    current1 = 0, player1 = 0, player0=1, remove(1), add(0);
+    checkWinner(gameState.score1,1)
+    gameState.current1 = 0, gameState.player1 = 0, gameState.player0=1, remove(1), add(0);
   }
-  
 });
