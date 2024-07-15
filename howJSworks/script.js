6d2c803a58d0a28562bd2fd03efa3c5b600b2233
victordhn <33136394+victordhn@users.noreply.github.com>
2024-07-15 02:39:40 -0300

    updated

--- howJSworks/script.js
+++ howJSworks/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Hoisting works only for Function Declarations
 
 console.log('before declaration', addDec(2, 3));
@@ -18,3 +20,15 @@ const addArr = (a,b) => a+b;
 console.log('after declaration', addDec(2, 3));
 console.log('after expression', addExp(2, 3));
 console.log('after arrow', addArr(2, 3));
+
+// THIS
+// utilizado em objetos, como um método
+
+const victor = {
+  year: 1991,
+  name: 'Victor',
+  calcAge: function () {
+    return 2024 - this.year
+  }
+}
+console.log(victor.calcAge());
