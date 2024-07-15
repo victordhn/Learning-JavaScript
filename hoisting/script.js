def3f2e4f6bca1350d229623cf4baf03de0ea936
victordhn <33136394+victordhn@users.noreply.github.com>
2024-07-15 02:25:59 -0300

    updated

---
+++ hoisting/script.js
@@ -0,0 +1,19 @@
+// Hoisting works only for Function Declarations
+
+console.log('before declaration', addDec(2, 3));
+// console.log('before expression', addExp(2, 3));
+// console.log('before arrow', addArr(2, 3));
+
+function addDec(a, b) {
+  return a + b;
+}
+
+const addExp = function (a,b) {
+    return a+b;
+}
+
+const addArr = (a,b) => a+b;
+
+console.log('after declaration', addDec(2, 3));
+console.log('after expression', addExp(2, 3));
+console.log('after arrow', addArr(2, 3));
